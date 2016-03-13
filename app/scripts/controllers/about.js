4f430114614c21d096186a14733925e84fa29e82
MikaelPhilip <philip.mikael@gmail.com>
2016-03-13 10:43:11 +0100

    Premier commit: porjet de base et premiere modification

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomanProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yomanProjectApp
+ */
+angular.module('yomanProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomanProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yomanProjectApp
+ */
+angular.module('yomanProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
